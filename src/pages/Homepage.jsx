b31b550d6dc552f0fda36479fd98761386d547e1
Juliet Njagi <njagijuliet02@gmail.com>
2025-10-19 07:13:55 +0300

    Worked on the footer

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../sections/Hero'
+import Footer from '../components/Footer'
 
 const Homepage = () => {
   return (
     <div className="">
       <Navbar />
       <Hero />
+      <Footer />
     </div>
   )
 }
